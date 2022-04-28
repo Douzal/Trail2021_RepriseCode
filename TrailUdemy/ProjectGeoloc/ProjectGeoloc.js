5ed5cb40534083cc36834d6d3f3e00e6cc4a1937
Douzal <alexis_masson_@hotmail.fr>
2022-04-28 18:52:05 +0200

    Ajout Geoloc et promises

---
+++ TrailUdemy/ProjectGeoloc/ProjectGeoloc.js
@@ -0,0 +1,129 @@
+// jQuery(document).ready(function($) {
+$(function() { // jQuery plus rapide
+
+  /* PARAMS URL */
+  const unitsParam        = 'metric';
+  const APIkey            = '05803e9fbc8c48d21dddf6073c12979f';
+  
+  /* PARAMS */
+  let ville               = 'Port Louis';
+  let villeChoisie        = ville;
+  let urlOWM              = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${APIkey}&units=${unitsParam}`;
+  
+  /* SELECTEURS */
+  let spVille             = $('#ville');
+  let spTemperatureLbl    = $('#temperature_label');
+  let [divChanger]        = $('#changer'); 
+
+
+  /* CREATE REQUEST */
+  let req = new XMLHttpRequest();
+  
+  function majTempEtVille() {
+    req.open('GET', urlOWM);
+    req.responseType = 'json';
+    req.send();
+    req.onload = () => {
+      if(req.readyState === XMLHttpRequest.DONE) {
+        if(req.status === 200) {
+          let resp = req.response;
+          spVille.text(resp.name);
+          spTemperatureLbl.text(resp.main.temp);
+        } else {
+          alert('--\tTemporary unable to join website and load datas\nPlease try again later --');
+        }
+      }
+    }
+  }
+
+  majTempEtVille();
+
+  /* MANAGE CLICK Changer Ville */
+  divChanger.addEventListener('click', (e) => {
+    e.preventDefault();
+    villeChoisie = prompt('De quelle ville souhaitez vous connaître la temp ?', 'Saint-Sauveur-sur-Tinée');
+    urlOWM              = `https://api.openweathermap.org/data/2.5/weather?q=${villeChoisie}&appid=${APIkey}&units=${unitsParam}`;
+    majTempEtVille();
+  });
+
+  /* PROMISES : add CSS */
+
+  // param : array de cssName
+  function chargerCSS(css) {
+    return(new Promise((resolve, reject) => {
+        /* CREATE THE LINK BALISE, FILL IT, INCLUDE IT ON HEAD */
+        let link = document.createElement('link');
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('href', css);
+        document.head.appendChild(link);
+        
+        /* PROMISE USE */
+        /* CASE SUCCESS */
+        link.onload = () => {
+          resolve(`Fichier ${link.href} successfuly loaded`);
+        }
+
+        /* CASE FAILED */
+        link.onerror = () => {
+          reject(new Error(`Fichier ${link.href} UNsuccessfuly loaded`));
+        }
+    }));
+  }
+
+  const prom = chargerCSS('ProjectGeoloc.css');
+  prom.then(
+    function (ds) {
+      console.log(ds);
+    },
+    function (err) {
+  console.log(err);      
+    }
+  );
+
+
+
+  function randomBetweenRange(min, max) {
+    return min + Math.floor(Math.floor(max-min+1)*Math.random());
+  }
+
+  /////
+  /* RECAP POUR VERIFIER L'ACQUIS : TEST DE CHARGEMNT D'UN SCRIPT */
+
+  function chargerScript(scriptName) {
+    return new Promise((ok, pasOk) => { // (resolve, reject)
+
+      /* create element */
+      let newScript = document.createElement('script');
+      // newScript.setAttribute('type', 'text/javascript');
+      // newScript.setAttribute('src', scriptName);
+      newScript.type  = 'text/javascript';
+      newScript.src   = scriptName;
+      document.head.append(newScript)
+
+      /* case resolve */
+      newScript.onload = () => ok (`\n\tOK load script : ${scriptName}`); 
+
+      /* case abort */
+      newScript.onerror = () => pasOk(new Error (`\n\tPAS OK load script : ${scriptName}`));
+    });
+
+  }
+
+  /* call promise */
+  const prometMoi = chargerScript('test.js');
+  prometMoi.then(
+    function (err) {
+      console.log(err);
+    },
+    function (res) {
+      console.log(`\n\tCASE ERR : ${res}`);
+    }
+  )
+
+  /* permet uniquement de log si erreur
+     nb : noter le console.log sans parenthèses*/
+  chargerScript('test.js').catch(console.log());
+    
+  ////
+
+});
--- TrailUdemy/ProjectVrac/vracJsConstructeursEtc.js
+++ TrailUdemy/ProjectVrac/vracJsConstructeursEtc.js
@@ -1,157 +1,159 @@
-jQuery(document).ready(function($) {
-    // console.log('Hello js');
-// objet constructeur avec prototype
+// jQuery(document).ready(function($) {
+$(function() { // jQuery plus rapide
+        // console.log('Hello js');
+    // objet constructeur avec prototype
 
-// création d'un objet voiture : roues, marque, année
-function Voiture(roues, marque, annee) {
-    this.roues = roues;
-    this.marque = marque;
-    this.annee = annee;
-}
-
-Voiture.prototype.seDecrire = function() {
-    console.log('Voiture\n\tmarque : '+this.marque+'\n\troues motrices : '+this.roues+'\n\tannée : '+this.annee);
-}
-
-let tesla = new Voiture('2', 'Tesla', '2020');
-// console.log(tesla);
-// tesla.seDecrire();
-
-// console.log(tesla.hasOwnProperty()); // function not founded in voiture() constructor, but from Object contstructor (inheritance)
-
-// Animal
-// function Animal(nbDePates, poids) {
-//     this.nbDePates = nbDePates;
-//     this.poids = poids;
-// }
-
-// function Mammifere(typePoils) {
-//     Animal.call(this, nbDePates, poids);
-//     this.typePoils = typePoils;
-// }
-
-
-// function Oiseau(nbDePates, poids, longueurAiles) {
-//     Animal.call(this, nbDePates, poids);
-//     this.longueurAiles = longueurAiles;
-// }
-
-// Animal.prototype.seDecrire = function () {
-//     console.log('je suis un oiseau, j\'ai '+this.nbDePates +' pates, je pèse '+this.poids+' et j\'ai des ailes de '+this.longueurAiles);
-// }
-
-// héritage et paramétrage des constructeurs
-// Oiseau.prototype = Object.create(Animal.prototype);
-// Oiseau.prototype.constructor = Oiseau;// Animal.prototype.constructor; // --> faix :()
-
-// tests et debugs..
-// let perr = new Oiseau('2', '15g', '20cm');
-// perr.seDecrire();
-// console.log(perr);
-
-
-///////// avec les classes
-class Animal {
-    constructor(nbPates, poids) {
-        this.nbPates = nbPates;
-        this.poids = poids;
+    // création d'un objet voiture : roues, marque, année
+    function Voiture(roues, marque, annee) {
+        this.roues = roues;
+        this.marque = marque;
+        this.annee = annee;
     }
 
-    get phrasePresentation() {
-        return 'Animal ayant '+this.nbPates+' pates, et pesant '+this.poids+'.';
+    Voiture.prototype.seDecrire = function() {
+        // console.log('Voiture\n\tmarque : '+this.marque+'\n\troues motrices : '+this.roues+'\n\tannée : '+this.annee);
+        console.log(`Voiture\n\tmarque : ${this.marque}\n\troues motrices : ${this.roues}\n\tannée : ${this.annee}`);
     }
 
-    sePresenter() {
-        console.log('Animal ayant '+this.nbPates+' pates, et pesant '+this.poids+'.');
+    let tesla = new Voiture('2', 'Tesla', '2020');
+    // console.log(tesla);
+    // tesla.seDecrire();
+
+    // console.log(tesla.hasOwnProperty()); // function not founded in voiture() constructor, but from Object contstructor (inheritance)
+
+    // Animal
+    // function Animal(nbDePates, poids) {
+    //     this.nbDePates = nbDePates;
+    //     this.poids = poids;
+    // }
+
+    // function Mammifere(typePoils) {
+    //     Animal.call(this, nbDePates, poids);
+    //     this.typePoils = typePoils;
+    // }
+
+
+    // function Oiseau(nbDePates, poids, longueurAiles) {
+    //     Animal.call(this, nbDePates, poids);
+    //     this.longueurAiles = longueurAiles;
+    // }
+
+    // Animal.prototype.seDecrire = function () {
+    //     console.log('je suis un oiseau, j\'ai '+this.nbDePates +' pates, je pèse '+this.poids+' et j\'ai des ailes de '+this.longueurAiles);
+    // }
+
+    // héritage et paramétrage des constructeurs
+    // Oiseau.prototype = Object.create(Animal.prototype);
+    // Oiseau.prototype.constructor = Oiseau;// Animal.prototype.constructor; // --> faix :()
+
+    // tests et debugs..
+    // let perr = new Oiseau('2', '15g', '20cm');
+    // perr.seDecrire();
+    // console.log(perr);
+
+
+    ///////// avec les classes
+    class Animal {
+        constructor(nbPates, poids) {
+            this.nbPates = nbPates;
+            this.poids = poids;
+        }
+
+        get phrasePresentation() {
+            return 'Animal ayant '+this.nbPates+' pates, et pesant '+this.poids+'.';
+        }
+
+        sePresenter() {
+            console.log('Animal ayant '+this.nbPates+' pates, et pesant '+this.poids+'.');
+        }
     }
-}
 
-class Oiseau extends Animal {
-    constructor(nbPates, poids, longueurAiles) {
-        super(nbPates, poids);
-        this.longueurAiles = longueurAiles;
+    class Oiseau extends Animal {
+        constructor(nbPates, poids, longueurAiles) {
+            super(nbPates, poids);
+            this.longueurAiles = longueurAiles;
+        }
+
+        get nombreDePates (){
+            return this.nbPates
+        }
+        
+        voler() {
+            console.log('je suis un oiseau, j\'ai '+this.nbPates +' pates, je pèse '+this.poids+' et j\'ai des ailes de '+this.longueurAiles)
+        }
     }
 
-    get nombreDePates (){
-        return this.nbPates
-    }
-    
-    voler() {
-        console.log('je suis un oiseau, j\'ai '+this.nbPates +' pates, je pèse '+this.poids+' et j\'ai des ailes de '+this.longueurAiles)
-    }
-}
+    let perr = new Oiseau(2, '20g', '20cm');
+    // perr.sePresenter();
+    // perr.voler();
+    // console.log('nbPates via Getter : '+perr.nombreDePates);
 
-let perr = new Oiseau(2, '20g', '20cm');
-// perr.sePresenter();
-// perr.voler();
-// console.log('nbPates via Getter : '+perr.nombreDePates);
+    class Poisson extends Animal {
+        constructor(pattes, pds, nbNageoires, taille) {
+            super(pattes, pds);
+            this.nbNageoires = nbNageoires;
+            this.taille = taille;
+        };
 
-class Poisson extends Animal {
-    constructor(pattes, pds, nbNageoires, taille) {
-        super(pattes, pds);
-        this.nbNageoires = nbNageoires;
-        this.taille = taille;
-    };
+        get poissPresentation (){
+            return 'Poisson ayant '+this.nbNageoires+' nageoires.';
+        }
 
-    get poissPresentation (){
-        return 'Poisson ayant '+this.nbNageoires+' nageoires.';
+        set changeNbNageoires(nb) {
+            this.nbNageoires = nb;
+        }
     }
 
-    set changeNbNageoires(nb) {
-        this.nbNageoires = nb;
+    let poisson = new Poisson(0, '10g', 4, '1m');
+    // console.log('poisson.poissPresentation :'+poisson.poissPresentation);
+    // console.log('poisson.phrasePresentation :'+poisson.phrasePresentation);
+    // console.log('poisson.sePresenter() :\n\t');
+    // poisson.sePresenter();
+
+    //////////////////////////////////////////////////
+    // LECON 149
+    let gandalf = {titre:"mage blanc"};
+
+    function direBonjour(arme, degat) {
+        console.log('Bonjour '+this.titre+', vous faites '+degat+' dégats avec votre '+arme);
     }
-}
 
-let poisson = new Poisson(0, '10g', 4, '1m');
-// console.log('poisson.poissPresentation :'+poisson.poissPresentation);
-// console.log('poisson.phrasePresentation :'+poisson.phrasePresentation);
-// console.log('poisson.sePresenter() :\n\t');
-// poisson.sePresenter();
+    // direBonjour.call(gandalf, 'baton', 200);
+    // direBonjour.apply(gandalf, ['baton', 200]);
+    // this.valeur = "Window";
 
-//////////////////////////////////////////////////
-// LECON 149
-let gandalf = {titre:"mage blanc"};
-
-function direBonjour(arme, degat) {
-    console.log('Bonjour '+this.titre+', vous faites '+degat+' dégats avec votre '+arme);
-}
-
-// direBonjour.call(gandalf, 'baton', 200);
-// direBonjour.apply(gandalf, ['baton', 200]);
-// this.valeur = "Window";
-
-let myObject = {
-    val: "Objet",
-    getVal: function () {
-        console.log(this.val)
+    let myObject = {
+        val: "Objet",
+        getVal: function () {
+            console.log(this.val)
+        }
     }
-}
 
-// console.log('myObject: '+myObject.getVal);
-// myObject.getVal();
-let maVal = myObject.getVal;
-// let maVal2 = myObject.getVal();
+    // console.log('myObject: '+myObject.getVal);
+    // myObject.getVal();
+    let maVal = myObject.getVal;
+    // let maVal2 = myObject.getVal();
 
-// console.log('\tmaVal : '+maVal); //+'\n\tmaVal2 : '+maVal2);
+    // console.log('\tmaVal : '+maVal); //+'\n\tmaVal2 : '+maVal2);
 
 
-// create a <span>
-let body = document.body;
-let span = document.createElement('span');
-span.innerHTML = 'yo js'
+    // create a <span>
+    let body = document.body;
+    let span = document.createElement('span');
+    span.innerHTML = 'yo js'
 
-$('span').attr('id', 'monSpan');
+    $('span').attr('id', 'monSpan');
 
-body.appendChild(span);
+    body.appendChild(span);
 
-span.innerHTML = fact(5);
-
-span.addEventListener('mouseover', function () {
-    span.innerHTML = fact2(4);
-});
-span.addEventListener('mouseout', function () {
     span.innerHTML = fact(5);
-});
+
+    span.addEventListener('mouseover', function () {
+        span.innerHTML = fact2(4);
+    });
+    span.addEventListener('mouseout', function () {
+        span.innerHTML = fact(5);
+    });
 
     // fonction exponentielle
     function fact (n) {
@@ -193,4 +195,83 @@ span.addEventListener('mouseout', function () {
 
     let tab = [1, 2, 3, 4, 5];
     // console.log('sumReducer [1, 2, 3, 4, 5] : '+sumReducer(tab));
+
+
+    ////// test AJAX
+    let div = document.createElement('div');
+    body.appendChild(div);
+
+    // fonction AJAX lancée par interval après
+    function ajaxBtcRequest() {
+
+        const url = 'https://blockchain.info/ticker';
+        let request = new XMLHttpRequest();
+        request.open('GET', url);
+        request.responseType = 'json';
+        request.send();
+        request.onload = function() {
+            // console.log(response.readyState);
+            if(request.readyState === XMLHttpRequest.DONE) {
+                if(request.status === 200) {
+                    let response = request.response;
+                    let actualPrice = response.EUR.last;
+                    div.textContent=actualPrice;
+                    console.log(actualPrice);
+                    console.log(`Reponse : ${JSON.stringify(response)[0]}`); // debile, ça ramène rien d'interessant
+                } else {
+                    console.log(`Reponse : ${JSON.stringify(response)[0]}`); // debile, ça ramène rien d'interessant
+                    console.log(`Erreur lors de la requête du prix - ${response.status}`); // useless
+                }
+            }
+
+        };
+
+    }
+
+    // setInterval(ajaxBtcRequest, 1000);
+
+
+    // requête POST
+    const urlPost = 'https://lesoublisdelinfo.com/api.php';
+ 
+    /* let requestPost = new XMLHttpRequest();
+    requestPost.open('GET', urlPost);
+    requestPost.responseType = 'json';
+    requestPost.send();
+
+    requestPost.onload = () => {
+        if(requestPost.readyState === requestPost.DONE) {
+            if(requestPost.status === 200){ // API accessible
+                let [div] = $('div');
+                let response = requestPost.response;
+                div.textContent = response.resultat;
+                console.log(`response : ${response}`);
+                console.log(`response.resultat : ${response.resultat}`);
+            } else {
+                console.log(`Erreur - retour de : ${response.resultat}`);
+            }
+        }
+    } */
+    
+    // rework de url Post pour vérifier les acquis 10:11
+    /* let reqPost = new XMLHttpRequest();
+    reqPost.open('POST', urlPost);
+    reqPost.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    reqPost.responseType = 'json';
+    reqPost.send('prenom=Alexis');
+
+    reqPost.onload = () => {
+        if(reqPost.readyState === XMLHttpRequest.DONE) {
+            if(reqPost.status === 200) {
+                // ok traitement 10:15
+                let resp = reqPost.response;
+                console.log(`resp : ${resp.prenom}`);
+
+            } else {
+                alert('try again later please');
+            }
+        }
+    }
+ */
+
 });
