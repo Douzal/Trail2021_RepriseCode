e7f40224dc6b29ef79c85fa42acab1e70f4cd19b
Douzal <alexis_masson_@hotmail.fr>
2022-05-24 17:39:22 +0200

    Ajout des animations sur le CV

--- TrailUdemy/FormationWeb/ProjectCV/script/CVjs.js
+++ TrailUdemy/FormationWeb/ProjectCV/script/CVjs.js
@@ -1,6 +1,6 @@
 $(function() {
+    'use strict';
     console.warn('CV bootstrap');
-    
 
     /* TOOLTIPS */
     let tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
@@ -9,10 +9,25 @@ $(function() {
     });
 
     /* allow focus when opening MODALS */
-    var myModal = document.getElementById('exampleModal');
-    var myInput = document.getElementById('myInput');
+    // var myModal = document.getElementById('exampleModal');
+    // var myInput = document.getElementById('myInput');
 
-    myModal.addEventListener('shown.bs.modal', function () {
-        myInput.focus();
+    // myModal.addEventListener('shown.bs.modal', function () {
+    //     myInput.focus();
+    // });
+
+    let img100 = $('.img100');
+    img100.mouseover(function() {
+        img100.animate({
+            left: '10px',
+            // width: '+=10px'
+        }, 200);
+    }).mouseout(function() {
+        img100.animate({
+            left: '-0.8px',
+            // width: '-=10px'
+        }, 200);
     });
+
+
 })
