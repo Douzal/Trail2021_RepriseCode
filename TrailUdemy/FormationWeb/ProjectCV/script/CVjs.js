2a03cb0db71a8ff996f5dc51b8cc3d56d318aabd
Douzal <alexis_masson_@hotmail.fr>
2022-05-25 09:16:06 +0200

    maj Fin CV

--- TrailUdemy/FormationWeb/ProjectCV/script/CVjs.js
+++ TrailUdemy/FormationWeb/ProjectCV/script/CVjs.js
@@ -3,31 +3,54 @@ $(function() {
     console.warn('CV bootstrap');
 
     /* TOOLTIPS */
-    let tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
+    // let tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    //     return new bootstrap.Tooltip(tooltipTriggerEl);
+    // });
 
-    /* allow focus when opening MODALS */
+    /* MODALS */
+    /*allow focus when opening MODALS */
     // var myModal = document.getElementById('exampleModal');
     // var myInput = document.getElementById('myInput');
 
     // myModal.addEventListener('shown.bs.modal', function () {
     //     myInput.focus();
     // });
+    
 
+    /* ANIMATE IMAGE */
     let img100 = $('.img100');
     img100.mouseover(function() {
+        img100.stop(true, true); // allows to clear queue : https://api.jquery.com/stop/
         img100.animate({
             left: '10px',
             // width: '+=10px'
         }, 200);
     }).mouseout(function() {
+        img100.stop(true, true);
         img100.animate({
             left: '-0.8px',
             // width: '-=10px'
         }, 200);
     });
+
+    /* ANIMATE FORMATION'S BLOCKS */
+    /* let animBlocks = $('.formation__block');
+    console.log([animBlocks]);
+    for(let block of animBlocks) {
+        console.log(block);
+        block.mouseover(function() {
+            animBlocks.animate({
+                left: '10px',
+                // width: '+=10px'
+            }, 200);
+        }).mouseout(function() {
+            animBlocks.animate({
+                left: '-0.8px',
+                // width: '-=10px'
+            }, 200);
+        });
+    } */
 
 
 })
